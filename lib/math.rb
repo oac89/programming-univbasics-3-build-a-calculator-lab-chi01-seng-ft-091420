f94247343a60c5964cbfe5c28515a7df88198ac4
Oscar A Castro <oacastro2@gmail.com>
2020-09-02 17:12:20 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,47 @@
 def addition(num1, num2)
-
+  #addition adds two numbers together do 
+  
+    addition = num2 + num1 
+  
+  addition 
+  
 end
 
-def subtraction(num1, num2)
 
+def subtraction(num1, num2)
+    
+      subtraction = num1 - num2 
+    
+    subtraction   
+    
 end
 
 def division(num1, num2)
+    
+      division = num1 / num2
+   
+    division 
 
 end
 
 def multiplication(num1, num2)
+    
+      multiplication = num1 * num2
+   
+  multiplication
 
 end
 
 def modulo(num1, num2)
-
+    
+      modulo = num1 % num2
+      
+    modulo
 end
 
 def square_root(num)
-
+    
+      square_root = Math.sqrt(num)
+      
+    square_root 
 end
---
+++ practice.rb
@@ -0,0 +1,7 @@
+def addition(num1, num2)
+  addition = (num1.to_i + num2.to_i)
+  num1 = 
+  num2 =
+  puts addition 
+end
+addition('7', '8')
