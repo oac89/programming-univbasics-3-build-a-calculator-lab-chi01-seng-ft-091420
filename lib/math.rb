e80f4b406b3890dcc5b8b15e66ed98c92652686c
Oscar A Castro <oacastro2@gmail.com>
2020-09-02 17:10:05 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -17,26 +17,31 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-    division = (num2 / num1)
+    
+      division = num2 / num1
    
-     division 
+    division 
 
 end
 
 def multiplication(num1, num2)
-    multiplication = (num1) * (num2)
+    
+      multiplication = num1 * num2
    
-    puts multiplication
+  multiplication
 
 end
 
 def modulo(num1, num2)
-    modulo = (num2 % num1)
-    p modulo
+    
+      modulo = num2 % num1
+      
+    modulo
 end
 
 def square_root(num)
-    (Math.sqrt( num ) )
+    
+      Math.sqrt(num)
+      
     square_root 
-    puts 
 end
