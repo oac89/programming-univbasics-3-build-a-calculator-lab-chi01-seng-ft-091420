14396dfe0faef5c222f4eed29c4743af7add1975
Oscar A Castro <oacastro2@gmail.com>
2020-09-02 16:41:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+  #addition adds two numbers together do 
+  addition = ( num2 + num1 )
+  num1 =
+  num2 =  
+  puts addition 
 end
+addition('5' '4')
+
 
 def subtraction(num1, num2)
-
+    subtraction == (num2 - num1)
+    puts subtraction
 end
 
 def division(num1, num2)
-
+    division == (num2 / num1)
+    puts division 
 end
 
 def multiplication(num1, num2)
-
+    multiplication == (num1 * num2)
+    puts multiplication
 end
 
 def modulo(num1, num2)
-
+    modulo == (num2 % num1)
+    puts modulo
 end
 
 def square_root(num)
-
+    square_root 
+    puts square_root
 end
